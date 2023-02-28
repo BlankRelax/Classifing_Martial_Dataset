73e7c1d145154601830d5b8b79280e7f6be61320
Hassaan <hassaan2009@gmail.com>
2023-02-28 14:53:37 +0000

    first

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
+def get_train_dat():
+    image_classes = np.loadtxt('C:\\Users\Hassaan\\PycharmProjects\\Classifing_Martial_Dataset\\labels-map-proj-v3.txt',dtype=str)
+    df = pd.DataFrame(image_classes, columns=['filename', 'class'])
+    idg = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255.)
+    train_ds = idg.flow_from_dataframe(dataframe=df,directory='C:\\Users\Hassaan\\PycharmProjects\\Classifing_Martial_Dataset\\map-proj-v3')
+    return(train_ds)
 
-image_classes = np.loadtxt('C:\\Users\Hassaan\\PycharmProjects\\Classifing_Martial_Dataset\\labels-map-proj-v3.txt', dtype=str)
-df = pd.DataFrame(image_classes,columns=['filename','class'])
-idg = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255.)
-train_ds = idg.flow_from_dataframe(dataframe=df, directory='C:\\Users\Hassaan\\PycharmProjects\\Classifing_Martial_Dataset\\map-proj-v3',batch_size=32)
 
+train_ds = get_train_dat()
+x_train= train_ds[0][0][0] # this prints the image, [0][0][32] is out of index because the batch size is 32
+# access the different batches from the first index of train_ds
+y_train= train_ds[0][1][0] # this tells us the class of the image
 
-img1= train_ds[0][0][0][0]
-img2=tf.reshape(img1,(32,24))
+print(train_ds[0][1][0])
 
-plt.imshow(img2, vmin=0,vmax=255)
+plt.imshow(train_ds[4][0][31])
 plt.show()
